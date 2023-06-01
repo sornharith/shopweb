71dbf394336a249057104f9652e5d295998f8f2a
Harith Phalangpatanakij <sornharith@Hariths-MacBook-Pro.local>
2023-06-01 23:04:03 +0700

    adding api of user and db

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from routers import book, user, file
+
+app = FastAPI()
+
+app.include_router(book.router)
+app.include_router(user.router)
+app.include_router(file.router)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
